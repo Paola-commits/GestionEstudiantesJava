03083388cee41b8262db384c38922ba08de48bdb
Paola-commits <paolaamadorys@gmail.com>
2025-09-14 13:53:22 -0600

    Create Estudiante.java
    Archivo base para clase Estudiante con atributos y método mostrarDatos.

---
+++ Estudiante.java
@@ -0,0 +1,14 @@
+public class Estudiante {
+    protected String nombre;
+    protected int edad;
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
